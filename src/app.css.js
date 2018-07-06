13f73f54a902c98380f3c4536a727629ddc7f606
Will Adkins <wkadki01@gmail.com>
2018-07-06 14:38:50 -0400

    Color Change to gold

--- src/app.css.js
+++ src/app.css.js
@@ -6,24 +6,14 @@ const style = {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    flexDirection: 'column',
-    textShadow: '3px 3px 3px light-blue'
+    flexDirection: 'column'
   },
   button: {
     backgroundColor: 'purple',
     color: 'white',
     padding: '16px',
     borderRadius: '4px',
-    border: '2px solid white',
-    textShadow: '2px 2px 2px light-blue'
-  },
-  box: {
-    backgroundColor: 'purple',
-    border: '3px solid white',
-    padding: '16px',
-    marginTop: '16px',
-    textShadow: '2px 2px 2px light-blue',
-    textAlign: 'center'
+    border: '2px solid white'
   }
 }
 
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import style from './app.css.js'
 import fortuneCookie from 'fortune-cookie'
+import logo from './cooki.png'
 
 const getRandomInt = max => Math.floor(Math.random() * Math.floor(max))
 
@@ -13,19 +14,19 @@ const getFortune = () => {
 const App = ({ state, setState }) => {
   return (
     <div style={style.root}>
+      <img height="100" src={logo} alt="fortune cookie logo" />
       <h1>Fortune Cookie App</h1>
-      <h3 className="mh2 fw1 ba pa4 br2 bg-light-blue">Do you dare partake?</h3>
+      {state.fortune ? (
+        <h3 className="mh2 fw1 ba pa4 br2 bg-gold">{state.fortune}</h3>
+      ) : (
+        <h3 className="mh2 fw1 ba pa4 br2 bg-gold">
+          Eat a cookie to find your fortune!
+        </h3>
+      )}
       <button style={style.button} onClick={() => setState(getFortune())}>
         Open a cookie
       </button>
-
-      <div>
-        {state.fortune ? (
-          <p>{state.fortune}</p>
-        ) : (
-          <p className="instructions">Have you opened your cookie yet?</p>
-        )}
-      </div>
+      <p className="instructions">Have you opened your cookie yet?</p>
     </div>
   )
 }
